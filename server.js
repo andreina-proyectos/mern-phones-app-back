3380a43db0d989e7dcb83d99311dc4b5121bbd51
andreina.romero <andreina.romero@viseo.com>
2021-04-10 18:18:44 +0200

    Add get all phone list endpoint and test it in postman. It works, but need to find a good devices data

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
 
+const phonesList = [
+  {
+   "id":  1,
+   "model": "nokia",
+   "manufacturer": "nokia.sa",
+   "img": "",
+   "description": "this is a description text about phone"
+  },
+  {
+    "id":  2,
+    "model": "iphone",
+    "manufacturer": "Apple",
+    "img": "",
+    "description": "this is a description text about phone"
+  },
+  {
+    "id":  3,
+    "model": "samsung galaxy",
+    "manufacturer": "Samsung",
+    "img": "",
+    "description": "this is a description text about phone"
+  }
+]
+
+//GET
 app.get('/', (req, res) => {
   res.send('Test andre!')
+})
+
+app.get('/phones', (req, res) => {
+  res
+    .status(200)
+    .json(phonesList)
 })
 
 app.listen(port, () => {
