6b5f4e4d7b4f85bd4c780b4e416cfdc5052387e1
andreina.romero <andreina.romero@viseo.com>
2021-04-10 18:23:19 +0200

    Export phones data to a new file

---
+++ data-service.js
@@ -0,0 +1,25 @@
+let phonesList = [
+    {
+     "id":  1,
+     "model": "nokia",
+     "manufacturer": "nokia.sa",
+     "img": "",
+     "description": "this is a description text about phone"
+    },
+    {
+      "id":  2,
+      "model": "iphone",
+      "manufacturer": "Apple",
+      "img": "",
+      "description": "this is a description text about phone"
+    },
+    {
+      "id":  3,
+      "model": "samsung galaxy",
+      "manufacturer": "Samsung",
+      "img": "",
+      "description": "this is a description text about phone"
+    }
+  ]
+
+  module.exports = phonesList;
--- server.js
+++ server.js
@@ -1,30 +1,7 @@
 const express = require('express')
+const phonesList = require("./data-service")
 const app = express()
 const port = 3000;
-
-const phonesList = [
-  {
-   "id":  1,
-   "model": "nokia",
-   "manufacturer": "nokia.sa",
-   "img": "",
-   "description": "this is a description text about phone"
-  },
-  {
-    "id":  2,
-    "model": "iphone",
-    "manufacturer": "Apple",
-    "img": "",
-    "description": "this is a description text about phone"
-  },
-  {
-    "id":  3,
-    "model": "samsung galaxy",
-    "manufacturer": "Samsung",
-    "img": "",
-    "description": "this is a description text about phone"
-  }
-]
 
 //GET
 app.get('/', (req, res) => {
